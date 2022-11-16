dcb913e6c644b3a676a19a36f57e8b4c43d38eab
nardor <nard0r@protonmail.com>
2022-11-16 20:52:02 +0100

    Sway IPC support Still has a bug with the window focus

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use actix::prelude::*;
-use actors::glimmer_manager::AttachSenderMsg;
+use actors::{glimmer_manager::AttachSenderMsg, i3_ipc::WindowManager};
 use clap::Parser;
 use std::thread;
 mod actors;
@@ -9,16 +9,29 @@ mod gtk_utils;
 struct Opts {
     #[arg(short, long, default_value = "./style.css")]
     styles: String,
+    #[clap(long)]
+    use_sway: bool,
 }
 
 fn main() {
     let opts = Opts::parse();
 
-    gtk::init().expect("Failed to initialize GTK.");
+    // x11 backend uses xwayland package
+    // it has better results than the wayland backend
+
+    // The following cannot be called before gdk::init()
+    // Most probably an oversight (https://github.com/gtk-rs/gtk3-rs/pull/791)
+    // gdk::set_allowed_backends("x11");
+
+    // Set env instead:
+    std::env::set_var("GDK_BACKEND", "x11");
+
+    gtk::init().expect("Failed to init GTK.");
+
     gtk_utils::setup(opts.styles);
 
     let sender = gtk_utils::handle_messages();
-
+    let sway = opts.use_sway;
     thread::spawn(move || {
         let system = System::new();
 
@@ -26,7 +39,14 @@ fn main() {
             let manager = actors::glimmer_manager::GlimmerManager::from_registry();
             manager.do_send(AttachSenderMsg { sender });
 
-            actors::i3_ipc::I3Ipc {}.start();
+            actors::i3_ipc::WmIPC {
+                wm: if sway {
+                    WindowManager::SWAY
+                } else {
+                    WindowManager::I3
+                },
+            }
+            .start();
         });
 
         system.run().unwrap();
